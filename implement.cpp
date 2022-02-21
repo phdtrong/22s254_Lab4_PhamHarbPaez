01538347cda12e01ffa448270d4ca8bd1e51b52d
phdtrong <phdtrong200748@gmail.com>
2022-02-21 18:56:39 +0000

    new file named implement.cpp updated

--- implement.cpp
+++ implement.cpp
@@ -1,21 +1,19 @@
-
 #include <iostream>
 #include <fstream>
 #include <string>
 #include "implement.h"
 using namespace std;
-int implement::read_me()
-{
+int implement::read_me() {
   string line;
   ifstream myfile ("README.md");
-  if (myfile.is_open())
-  {
+  if (myfile.is_open()) {
     while ( getline (myfile,line) )
       cout << line << '\n';
     myfile.close();
-		return 1;
-  }
-  else 
-		cout << "Unable to open file"; 
 		return 0;
+  }
+  else {
+		cout << "Unable to open file"; 
+		return -1;
+	}
 }
