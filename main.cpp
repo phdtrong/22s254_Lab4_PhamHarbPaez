31a2a066bbe0826dce1d1cc23197f8bdeed9bcaf
phdtrong <70780829+phdtrong@users.noreply.github.com>
2022-02-26 10:13:31 -0800

    Delete main.cpp
    Refreshing files

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "implement.h"
-using namespace std;
-int main () 
-{
-	implement my_imp;
-	cout << "Content of read_me file" << endl;
-	return my_imp.read_me();//test2
-}
